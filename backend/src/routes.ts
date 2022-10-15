34bd3c98fb5dc5b5b1f6d9be9132b229e70b6986
Fernando Medeiros <fernandohenrique520@live.com>
2022-10-15 02:59:50 -0300

    hot fix backend

--- backend/src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from "express";
-
-const router = Router();
-
-router.get('/teste', (req: Request, res: Response) => {
-    return res.json({ ok : true})
-})
-
-export { router };
